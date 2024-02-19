d6ce470b109a2b59741db4843088abecb46442a7
SimonTse123 <simon.tse25@ycdsbk12.ca>
2024-02-19 12:54:19 -0500

    modified:   AreaSin.java 	modified:   FtoC.java

--- FtoC.java
+++ FtoC.java
@@ -1,30 +1,26 @@
 class FtoC extends ConsoleProgram {
 
   /**
-  * A program AreaSin.java that uses the Math.sin() function to calculate the area of a triangle, given two sides and the contained angle.
+  * A program FtoC.java that lets you enter a degree measure in Fahrenheit, and that prints the result in celsius degree measure.
   * @author: Simon Tse
   */
   
   public void run() {
     
     // Declare variables
-    double dblSideOne;
-    double dblSideTwo;
-    double dblDegree;
-    double dblRadians;
-    double dblArea;
+    int intTempFar;
+    int intTempCel;
+    
 
     // Get variables
-    dblSideOne = readDouble("Enter the value for the longer length of the triangle: ");
-    dblSideTwo = readDouble("Enter the value for the shorter length of the triangle: ");
-    dblDegree = readDouble("Enter the value for the contained angle of the triangle in degrees: ");
+    intTempFar = readInt("Enter the temperature in Fahrenhiet: ");
 
-    // Calculate for the area
-    dblRadians = Math.PI * dblDegree / 180;
-    dblArea = dblSideOne * dblSideTwo * Math.sin(dblRadians) / 2;
+    // Calculate for the temperature in celsius
+    intTempCel = (intTempFar - 32) * 5 / 9;
+    
 
     // Show result
-    System.out.println("The area of the triangle is: " + dblArea);
+    System.out.println("The temperature you enter in celsius is " + intTempCel + "°C");
     
   }
 }
